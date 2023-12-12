b6b26cc9e2269e507a420294e81dbc497534c204
Rifat Rabbi <rifayrabby78@gmail.com>
2023-12-12 18:35:57 +0600

    updated feature admin can make employee to hr or employee and can rejoin an user or employee

--- src/api/users/fireUser.js
+++ src/api/users/fireUser.js
@@ -3,12 +3,24 @@ const userCollection = require("../../models/UserModel");
 const fireUser = async (req, res) => {
   const email = req.params.email;
   const query = { email: email };
-  const updateDoc = {
-    $set: {
-      fire: true,
-    },
-  };
-  const result = await userCollection.updateOne(query, updateDoc);
-  res.send(result);
+  const result = await userCollection.findOne(query);
+  if (result.fire) {
+    const updateDoc = {
+      $set: {
+        fire: false,
+      },
+    };
+    const result2 = await userCollection.updateOne(query, updateDoc);
+    res.send(result2);
+  }
+  if (!result.fire) {
+    const updateDoc = {
+      $set: {
+        fire: true,
+      },
+    };
+    const result2 = await userCollection.updateOne(query, updateDoc);
+    res.send(result2);
+  }
 };
 module.exports = fireUser;
--- src/api/users/makeHR.js
+++ src/api/users/makeHR.js
@@ -3,12 +3,24 @@ const userCollection = require("../../models/UserModel");
 const makeHR = async (req, res) => {
   const email = req.params.email;
   const query = { email: email };
-  const updateDoc = {
-    $set: {
-      role: "HR",
-    },
-  };
-  const result = await userCollection.updateOne(query, updateDoc);
-  res.send(result);
+  const result = await userCollection.findOne(query);
+  if (result.role === "Employee") {
+    const updateDoc = {
+      $set: {
+        role: "HR",
+      },
+    };
+    const result2 = await userCollection.updateOne(query, updateDoc);
+    res.send(result2);
+  }
+  if (result.role === "HR") {
+    const updateDoc = {
+      $set: {
+        role: "Employee",
+      },
+    };
+    const result2 = await userCollection.updateOne(query, updateDoc);
+    res.send(result2);
+  }
 };
 module.exports = makeHR;
